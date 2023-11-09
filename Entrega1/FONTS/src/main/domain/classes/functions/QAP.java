771c95870532e7eed8507ba7c8b8d652583be190
Oriol Ramos <oramos@livitum.com>
2023-11-09 20:05:11 +0100

    funciones QAP

--- Entrega1/FONTS/src/main/domain/classes/functions/QAP.java
+++ Entrega1/FONTS/src/main/domain/classes/functions/QAP.java
@@ -25,7 +25,7 @@ public class QAP {
         generarMatrizDistancias();
     }
 
-    // ASSIGNACIÓN ALEATORIA DE M TECLAS A M POSICIONES; DE MOMENTO NECESITAMOS QUE HAYA N*N TELAS (LLENARLO ENTERO)
+    /**Assignació aleatòria de m tecles a m posicions; de moment l'omplim tot sencer (12 posicions = 12 lletres)*/
     public void calcularAsignacionAleatoria(List<Character> teclas) {
         if(teclas.size() != filas * columnas) {
             throw new IllegalArgumentException("El número de teclas debe coincidir con el número de posiciones en el teclado.");
@@ -44,6 +44,8 @@ public class QAP {
         }
     }
 
+    /**Genera matriu de frequències, li passem la llista de parells de lletres amb les frequencies (ordenades?)
+     * i la llista de tecles.*/
     public void generarMatrizDeFrecuencias(List<PairFrequency> frecuenciasPares, List<Character> teclas) {
         // Primero, mapear cada letra a su índice en la matriz del teclado
         int index = 0;
@@ -69,13 +71,14 @@ public class QAP {
 
             if (indice1 != null && indice2 != null) {
                 matrizFrecuencias[indice1][indice2] = frecuencia;
-                matrizFrecuencias[indice2][indice1] = frecuencia; // Asumiendo que la relación es bidireccional y simétrica
+                matrizFrecuencias[indice2][indice1] = frecuencia;
             }
         }
     }
 
+    /**Algoritme greedy per la sol ini*/
     public void calcularAsignacionGreedy(List<PairFrequency> frecuenciasPares, List<Character> teclas) {
-        // Llama a generarMatrizDeFrecuencias antes de generar la solución codiciosa
+        // Hem de veure si la cridem aquí o si ja la haurem cridat abans
         generarMatrizDeFrecuencias(frecuenciasPares, teclas);
 
         // Genera una lista de índices basada en la frecuencia total de cada tecla
@@ -102,7 +105,7 @@ public class QAP {
 
         for (int i = inicioFila; i < inicioFila + filas; i++) {
             for (int j = inicioColumna; j < inicioColumna + columnas; j++) {
-                // Verifica si el índice está dentro de los límites del array
+                // Verifica los límites del array
                 if (i >= 0 && i < filas && j >= 0 && j < columnas) {
                     // Asigna la tecla a la posición actual del teclado
                     teclado[i][j] = teclas.get(indiceTecla);
@@ -124,6 +127,7 @@ public class QAP {
         // Itera sobre todos los pares de teclas en el teclado
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
+                /** Índex 1 és per si fem servir la funció de matriuDistancies. */
                 //int indice1 = letraAIndice.get(teclado[i][j]);
                 for (int k = 0; k < filas; k++) {
                     for (int l = 0; l < columnas; l++) {
@@ -131,6 +135,7 @@ public class QAP {
                         int distancia = Manhattan.calcularDistancia(i, j, k, l);
                         int frecuencia = matrizFrecuencias[letraAIndice.get(teclado[i][j])][letraAIndice.get(teclado[k][l])];
                         puntuacion += distancia * frecuencia;
+                        /** Índex 2 i puntuació igual, per si fem servir la funció, ja ho mirarem. */
                         //int indice2 = letraAIndice.get(teclado[k][l]);
                         //puntuacion += matrizDistancias[indice1][indice2] * matrizFrecuencias[indice1][indice2];
                     }
@@ -141,6 +146,7 @@ public class QAP {
         return puntuacion;
     }
 
+    /**Genera la matriu de distàncies de Manhattan, de moment no la fem servir perquè donava diferent, cal revisar*/
     private void generarMatrizDistancias() {
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
