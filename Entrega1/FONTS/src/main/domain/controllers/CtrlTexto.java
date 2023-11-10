a5f5d7ef689c0601127ee25ec33f89d58dc8e807
Alex <alexiamg@DESKTOP-U80D3IO>
2023-11-10 16:11:35 +0100

    Controladoesorigin

--- Entrega1/FONTS/src/main/domain/controllers/CtrlTexto.java
+++ Entrega1/FONTS/src/main/domain/controllers/CtrlTexto.java
@@ -1,9 +1,32 @@
 package main.domain.controllers;
 
+import main.domain.classes.Texto;
+import main.domain.classes.ConjuntoTextos;
+import main.domain.classes.ConjuntoTeclados;
+
 /**
  * Este controlador se encarga de gestionar los métodos de añadir, modificar i borrar alfabetos
  * Los alfabetos existentes quedarán dentro de un ...
  *
  */
 public class CtrlTexto {
+
+    // ---------- PARÁMETROS ----------
+    /**
+     * Parámetros de la clase Alfabeto y ConjuntoAlfabetos
+     */
+    private Texto TextoExistente;
+    private ConjuntoTextos CjtTextos;
+    private ConjuntoTeclados CjtTeclados;
+
+    /**
+     * Asigna un alfabeto y un cjt de alfabetos vacío
+     */
+    public CtrlTexto(){
+        CjtTextos = null;
+        TextoExistente = null;
+    }
+
+
+
 }
