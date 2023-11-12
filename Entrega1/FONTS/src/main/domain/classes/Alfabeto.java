53b5c6b8c21659a08d3fc5afa9bc857f2c41a152
JuliaTena <juliatena@JULIA>
2023-11-12 22:21:06 +0100

    test alfabeto

--- Entrega1/FONTS/src/main/domain/classes/Alfabeto.java
+++ Entrega1/FONTS/src/main/domain/classes/Alfabeto.java
@@ -72,6 +72,7 @@ public class Alfabeto // implements Comparable<Cela>, Serializable
     }
 
 
+
     /** ho comento però no ho elimino, serà inetressant fer metode toString
     @Override
     public String toString() {return _valorUsuari;}
