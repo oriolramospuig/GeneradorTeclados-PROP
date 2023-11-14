3da48dd9e182ca45e31adbdfc0c5ff0bee00d981
JuliaTena <juliatena@JULIA>
2023-11-14 23:55:18 +0100

    implementacion conjunto teclado

---
+++ Entrega1/FONTS/src/test/TestConjuntoTeclados.java
@@ -0,0 +1,21 @@
+package test;
+
+
+import main.domain.classes.ConjuntoTeclados;
+import main.domain.classes.Teclado;
+import org.junit.Before;
+
+public class TestConjuntoTeclados {
+    private ConjuntoTeclados teclados;
+
+    private Teclado teclado1;
+
+    private Teclado teclado2;
+
+    @Before
+    public void setUp() {
+        teclados =
+    }
+
+
+}
