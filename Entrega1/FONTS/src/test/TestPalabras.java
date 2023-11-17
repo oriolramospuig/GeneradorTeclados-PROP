0714d3145cf0dbd88dd93db58f312726d50ddd6a
c0nd3neto <vmorenovillanueva15@gmail.com>
2023-11-17 17:14:17 +0100

    TestAsociacionTexto(comentat)

--- Entrega1/FONTS/src/test/TestPalabras.java
+++ Entrega1/FONTS/src/test/TestPalabras.java
@@ -45,12 +45,21 @@ public class TestPalabras
     @Test
     public void TestTratarEntrada() {
         System.out.println("Test TratarEntrada");
-
+        /*
         palabras.tratarEntrada();
 
         assertEquals(2, palabras.getFrecuenciaLetras().get("ai").intValue());
         assertEquals(2, palabras.getFrecuenciaLetras().get("ia").intValue());
         assertEquals(1, palabras.getFrecuenciaLetras().get("ac").intValue());
         assertEquals(1, palabras.getFrecuenciaLetras().get("ca").intValue());
+        */
+        palabras.tratarEntrada();
+
+        // Verificar que la frecuencia de pares de letras se haya calculado correctamente
+        Map<String, Integer> frecuenciaLetras = palabras.getFrecuenciaLetras();
+        assertEquals(2, frecuenciaLetras.containsKey("ab"));
+        assertEquals(3, frecuenciaLetras.get("ba"));
+        assertEquals(2, frecuenciaLetras.get("ac"));
+        assertEquals(1, frecuenciaLetras.get("ca"));
     }
 }
