c8704c180c51011a0cfc1740c7e2a6b888e5cadd
c0nd3neto <vmorenovillanueva15@gmail.com>
2023-11-17 18:24:13 +0100

    TestMatrices

---
+++ Entrega1/FONTS/src/test/functions/TestMatrices.java
@@ -0,0 +1,42 @@
+package test.functions;
+import main.domain.classes.functions.Matrices;
+
+import java.util.ArrayList;
+
+import main.domain.classes.Teclado;
+import main.domain.classes.functions.Manhattan;
+import org.junit.*;
+
+import java.io.*;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
+
+import java.lang.IndexOutOfBoundsException;
+import java.lang.NegativeArraySizeException;
+
+
+public class TestMatrices {
+    public void testGenerarMatrizDistancias() {
+        int filas = 3;
+        int columnas = 3;
+        int[][] matrizDistancias = new int[filas * columnas][filas * columnas];
+
+        // Llamar a la función para generar la matriz de distancias
+        Matrices.generarMatrizDistancias(filas, columnas, matrizDistancias);
+
+        // Verificar que la matriz de distancias se haya generado correctamente
+        for (int i = 0; i < filas * columnas; i++) {
+            for (int j = 0; j < filas * columnas; j++) {
+                // Verificar que la distancia sea la calculada por la función Manhattan
+                assertEquals(Manhattan.calcularDistancia(i / columnas, i % columnas, j / columnas, j % columnas),
+                        matrizDistancias[i][j]);
+            }
+        }
+    }
+}
