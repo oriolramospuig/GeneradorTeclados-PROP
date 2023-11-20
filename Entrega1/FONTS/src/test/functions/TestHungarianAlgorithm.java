673d44b1a01bd08c162560dc57f1added165e9ea
oriol.ramos.puig <oriol.ramos.puig@estudiantat.upc.edu>
2023-11-20 20:02:33 +0100

    hungarian bucle infinito

--- Entrega1/FONTS/src/test/functions/TestHungarianAlgorithm.java
+++ Entrega1/FONTS/src/test/functions/TestHungarianAlgorithm.java
@@ -2,6 +2,8 @@
 package test.functions;
 
 import main.domain.classes.functions.HungarianAlgorithm;
+import org.junit.Test;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.*;
@@ -9,14 +11,29 @@ import java.util.*;
 import static org.junit.Assert.*;
 
 public class TestHungarianAlgorithm {
+    @Test
     public void testHungarian() {
         HungarianAlgorithm hungarianAlgorithm = new HungarianAlgorithm();
         int[][] c1c2 = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
+                {21, 34, 31, 43},
+                {20, 35, 32, 44},
+                {20, 34, 33, 45},
+                {21, 34, 31, 43}
         };
         int result = hungarianAlgorithm.Hungarian(c1c2);
-        assertEquals(0, result);
+        assertEquals(128, result);
+    }
+
+    @Test
+    public void testNlineas() {
+        HungarianAlgorithm hungarianAlgorithm = new HungarianAlgorithm();
+        int[][] c1c2 = {
+                {0, 0, 0, 0},
+                {0, 2, 2, 2},
+                {0, 1, 3, 3},
+                {0, 0, 0, 0}
+        };
+        boolean result = hungarianAlgorithm.isComplete(c1c2);
+        assertEquals(false, result);
     }
 }
