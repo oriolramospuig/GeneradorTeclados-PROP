21d44e65c6012c1d1cd84983ac33749b15d543f2
34696 <oriolrp123@gmail.com>
2023-11-21 00:59:13 +0100

    ctrl nuevo

---
+++ Entrega1/FONTS/src/main/domain/controllers/CtrlTecladoQAP.java
@@ -0,0 +1,25 @@
+package main.domain.controllers;
+
+import main.domain.classes.Alfabeto;
+import main.domain.classes.Algoritmo;
+import main.domain.classes.AsociacionTextos;
+import main.domain.classes.Teclado;
+import main.domain.classes.types.PairFrequency;
+import main.domain.classes.types.PairInt;
+
+import java.util.ArrayList;
+
+public class CtrlTecladoQAP {
+    public Teclado crearTeclado(String nomT, AsociacionTextos asociacionTextos, Alfabeto alfabeto, Algoritmo algoritmo) {
+        PairInt dimensiones;
+        //calcular dim
+        Teclado teclado = new Teclado(nomT, asociacionTextos, alfabeto, algoritmo,dimensiones);
+
+        //generar matrius
+        ArrayList<PairFrequency> frecuencias = asociacionTextos.getFrecuenciaLetrasArray();
+
+        //cridarem qap
+        //reconstruir solució (números a lletres)
+        return teclado;
+    }
+}
