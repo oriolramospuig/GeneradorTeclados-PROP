a9471176fc766a30afce58d830442f9bfbfad59c
oriol.ramos.puig <oriol.ramos.puig@estudiantat.upc.edu>
2023-11-20 21:58:50 +0100

    driver prova uri modificat perque sigui només de lgoritme

--- Entrega1/FONTS/src/drivers/DriverProvaUri.java
+++ Entrega1/FONTS/src/drivers/DriverProvaUri.java
@@ -28,84 +28,6 @@ public class DriverProvaUri {
         conjuntoAlfabetos = new ConjuntoAlfabetos();
     }
 
-    public void agregarAlfabetoPorTerminal() {
-        System.out.println("Introduce el nombre del alfabeto:");
-        String nombre = inOut.leerString();
-        System.out.println("Introduce los caracteres del alfabeto separados por espacio (ejemplo: a b c ...):");
-        String entradaCaracteres = inOut.leerString();
-
-        // Verifica que la entrada de caracteres es válida.
-        if (inOut.contenidoValido(entradaCaracteres)) {
-            // Convierte la entrada en un ArrayList de caracteres.
-            ArrayList<Character> caracteres = new ArrayList<>();
-            for (char c : entradaCaracteres.toCharArray()) {
-                // Solo agrega caracteres no espacios.
-                if(c != ' ') {
-                    caracteres.add(c);
-                }
-            }
-            // Agrega el alfabeto al conjunto si es válido.
-            Alfabeto alfabeto = new Alfabeto(nombre, caracteres);
-            conjuntoAlfabetos.agregarAlfabeto(nombre, alfabeto);
-        } else {
-            System.out.println("El contenido introducido no es válido. Asegúrate de que sean caracteres separados por un espacio.");
-        }
-    }
-
-    /*public void agregarAlfabetoPorArchivo() {
-        System.out.println("Introduce el nombre del archivo:");
-        String nombreArchivo = inOut.leerString();
-        try {
-            ArrayList<Character> caracteres = inOut.leerCaracteresDeArchivo(nombreArchivo);
-            System.out.println("Introduce el nombre del alfabeto:");
-            String nombre = inOut.leerString();
-            Alfabeto alfabeto = new Alfabeto(nombre, caracteres);
-            conjuntoAlfabetos.agregarAlfabeto(nombre, alfabeto);
-            System.out.println("Alfabeto agregado con éxito desde el archivo: " + nombreArchivo);
-        } catch (FileNotFoundException e) {
-            System.out.println("El archivo no se encontró: " + nombreArchivo);
-        } catch (IllegalArgumentException e) {
-            System.out.println("El contenido del archivo no es válido: " + e.getMessage());
-        }
-    }
-
-     */
-
-
-    public void imprimirAlfabetos() {
-        // Obtén todos los alfabetos en el conjunto
-        HashMap<String, Alfabeto> alfabetos = conjuntoAlfabetos.getAlfabetos();
-        //ArrayList<String> alfabetos = conjuntoAlfabetos.getNombresAlfabetos();
-
-        // Verifica si hay alfabetos en el conjunto
-        if (alfabetos.isEmpty()) {
-            System.out.println("No hay alfabetos para mostrar.");
-            return;
-        }
-
-        // Itera sobre el conjunto de alfabetos e imprime la información de cada uno
-        for (HashMap.Entry<String, Alfabeto> entry : alfabetos.entrySet()) {
-            // Obtiene el nombre y el alfabeto del conjunto
-            String nombre = entry.getKey();
-            Alfabeto alfabeto = entry.getValue();
-            ArrayList<Character> contenido = alfabeto.getLetras();
-
-            // Imprime el nombre y el contenido del alfabeto
-            System.out.println("Nombre del alfabeto: " + nombre);
-            System.out.print("Contenido del alfabeto: ");
-
-            // Imprime el contenido del alfabeto, si no es nulo y tiene caracteres
-            if (contenido != null && !contenido.isEmpty()) {
-                for (char c : contenido) {
-                    System.out.print(c + " ");
-                }
-            } else {
-                System.out.print("El alfabeto está vacío o no se ha inicializado.");
-            }
-            System.out.println();
-        }
-    }
-
     void imprimirPruebaQAP() {
         System.out.println("Se generará un teclado aleatorio de 3*3 con las letras de la A a la I: ");
         System.out.println();
@@ -138,7 +60,7 @@ public class DriverProvaUri {
         int nc = 4;
 
         /*aixo serveix per enlloc de tractar lletres a, b, c ... tractar números 1, 2, 3... a la tornada de l'algoritme
-        * ho tornem a lletres. No se si es necessari tenir-ho, diria que no*/
+        * ho tornem a lletres.*/
         HashMap<Character, Integer> letraAIndice = new HashMap<>();
         for (int i = 0; i < teclas.size(); i++) {
             letraAIndice.put(teclas.get(i), i);
@@ -151,8 +73,6 @@ public class DriverProvaUri {
         Matrices.generarMatrizDistancias(nf,nc,matrizDistancias);
         /*creadora de la classe qap nova, per que poguem provar amb lo que ha dit a classe*/
         QAP qap = new QAP(nf, nc, matrizFrecuencias, matrizDistancias);
-        /*creadora antiga, amb això anava i donava resultat, la canviem per a canviar l'estrcutura del qap*/
-         //QAP qap = new QAP(nf, nc, teclas, frecuenciasPares);
     }
 
 
@@ -160,51 +80,9 @@ public class DriverProvaUri {
     public static void main(String[] args) {
         DriverProvaUri driver = new DriverProvaUri();
         muestraMetodos();
-        // System.out.println("Selecciona el método para agregar el alfabeto (1 - Terminal, 2 - Archivo):");
         String metodo = driver.inOut.leerString();
         while (!metodo.equals("0") && !metodo.equals("Salir")) {
             switch (metodo) {
-                case "1":
-                case "AlfabetoPorTerminal": {
-                    driver.agregarAlfabetoPorTerminal();
-                    break;
-                }
-                case "2":
-                case "AlfabetoPorArchivo": {
-                    //driver.agregarAlfabetoPorArchivo();
-                    break;
-                }
-                case "3":
-                case "ImprimirAlfabetos": {
-                    driver.imprimirAlfabetos();
-                    break;
-                }
-                case "4":
-                case "TextoPorTerminal": {
-                    //driver.agregarTextoPorTerminal();
-                    break;
-                }
-                case "5":
-                case "TextoPorArchivo": {
-                    // driver.agregarTextoPorArchivo();
-                    break;
-                }
-
-                case "6":
-                case "ImprimirTextos": {
-                    // driver.imprimirTextos();
-                    break;
-                }
-                case "7":
-                case "CrearAsociacionTextos": {
-                    // driver.crearAsociacionTextos();
-                    break;
-                }
-                case "8":
-                case "ImprimirAsociaciones": {
-                    // driver.imprimirAsociaciones();
-                    break;
-                }
                 case "9":
                 case "PruebaQAP": {
                     driver.imprimirPruebaQAP();
@@ -218,20 +96,11 @@ public class DriverProvaUri {
             driver.volverMenu();
             muestraMetodos();
             metodo = driver.inOut.leerString();
-            // MIRAR DE FER SERVIR SCANNER I NEXT LINE
         }
         driver.inOut.cerrarScanner();
     }
 
     private static void muestraMetodos() {
-        System.out.println("(1|AlfabetoPorTerminal) - Añadir Alfabeto");
-        System.out.println("(2|AlfabetoPorArchivo) - Añadir Alfabeto");
-        System.out.println("(3|ImprimirAlfabetos) - Imprimir Alfabetos");
-        System.out.println("(4|TextoPorTerminal) - Añadir Texto");
-        System.out.println("(5|TextoPorArchivo) - Añadir Texto");
-        System.out.println("(6|ImprimirTextos) - Imprimir Textos");
-        System.out.println("(7|CrearAsociacionTextos) - Crear Asociación Textos");
-        System.out.println("(8|ImprimirAsociaciones) - Imprimir Asociaciones");
         System.out.println("(9|PruebaQAP) - Prueba QAP");
         System.out.println();
         System.out.println("(0|Salir) - Cerrar Driver");
