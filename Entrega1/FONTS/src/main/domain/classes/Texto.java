8d8dfa2c598af8bc6c49bc47ef6d5a00086812ee
JuliaTena <juliatena@JULIA>
2023-11-21 13:11:56 +0100

    comentarios texto

--- Entrega1/FONTS/src/main/domain/classes/Texto.java
+++ Entrega1/FONTS/src/main/domain/classes/Texto.java
@@ -8,7 +8,6 @@ import java.util.HashMap;
  * Classe que representa un texto
  * @author X (X@estudiantat.upc.edu)
  */
-
 public abstract class Texto
 {
     // ---------- ATRIBUTOS COMUNES ----------
@@ -18,7 +17,7 @@ public abstract class Texto
     /** Guarda la lista de pares de carácteres y su frecuencia ("ab", 5) */
     protected HashMap<String, Integer> frecuenciaLetras;
 
-    /** Guarda las asociaciones que tiene este texto en su asociación */
+    /** Guarda el nombre de las asociaciones de textos que tienen este texto en su asociación */
     protected ArrayList<String> asociacionesVinculadas;
 
 
@@ -33,37 +32,40 @@ public abstract class Texto
 
     // ---------- GETTERS ----------
     /**
-     * Retorna el nombre introducido por el usuario
-     * @return String : Nombre introducido por el usuario
+     * Devuelve el nombre introducido por el usuario
+     * @return String: Nombre introducido por el usuario
      */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Devuelve el map de pares de letras con sus frecuencias
+     * @return HashMap<String,Integer>: Map de frecuenciaLetras
+     */
     public HashMap<String, Integer> getFrecuenciaLetras() {
         return frecuenciaLetras;
     }
 
     /**
-     * Retorna lista de nombres de las asociaciones de textos que tienen el texto
-     * @return ArrayList<String> : Lista de los nombres de las asociaciones vinculadas al texto
+     * Devuela una lista de nombres de las asociaciones de textos que contienen el texto
+     * @return ArrayList<String>: Lista de los nombres de las asociaciones vinculadas al texto
      */
     public ArrayList<String> getAsociacionesVinculadas() {
         return asociacionesVinculadas;
     }
+
     /**
-     * Retorna el contenido del texto
-     * @return String : Contenido del texto usado por las funciones, dependiendo del tipo de Texto el formato variará
+     * Devuelve el contenido del texto
+     * @return String: Contenido del texto, ya sea del tipo Palabra o Frecuencia
      */
     public abstract String getTexto();
 
 
     // ---------- SETTERS ----------
     /**
-     * No retorna.
+     * No devuelve nada. Añade a la lista de asociaciones vinculadas el nombre de la asociación de textos pasada por parámetro nomAT
      * @param nomAT nombre de la  asocación de textos a vincular
-     * Añade a la lista de asociaciones vinculadas el nombre de la asociación de textos
-     * pasada por parámetro nomAT
      */
     public void agregarAsociacionesVinculadas(String nomAT) {
         asociacionesVinculadas.add(nomAT);
@@ -71,11 +73,10 @@ public abstract class Texto
 
 
     // ---------- AUXILIARES ----------
+    //Segunda entrega
     /**
-     * No retorna.
+     * No devuelve nada. Borra de la lista de asociaciones de textos vinculadas el nombre de la asociación pasada por parámetro nomAT
      * @param nomAT nombre de la asociación de textos a desvincular
-     * Borra de la lista de asociaciones de textos vinculadas el nombre de la
-     * asociación pasada por parámetro nomAT
      */
     public void borrarAsociacionesVinculadas(String nomAT) {
     asociacionesVinculadas.remove(nomAT);
