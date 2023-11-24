0a2499ffc25adcd58670eee26fc200d58686d15e
c0nd3neto <vmorenovillanueva15@gmail.com>
2023-11-24 18:06:58 +0100

    ComentarisTest

--- Entrega1/FONTS/src/test/TestTeclado.java
+++ Entrega1/FONTS/src/test/TestTeclado.java
@@ -2,23 +2,11 @@ package test;
 
 import main.domain.classes.*;
 
-import java.util.ArrayList;
-
 import main.domain.classes.types.PairInt;
 import org.junit.*;
 
-import java.io.*;
-import java.util.Set;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertNotNull;
-
-import java.lang.IndexOutOfBoundsException;
-import java.lang.NegativeArraySizeException;
 
 public class TestTeclado {
 
@@ -28,23 +16,23 @@ public class TestTeclado {
      * Valores estudiados: Estrategia caja gris. Se define un teclado a partir de una asociación de texto y un alfabeto y se comprueba que los que guardo sean los mismos.
      * Operativa: Creamos un nuevo Teclado con los parámetros mencionados y escogemos un algoritmo y unas dimensiones para la creación de este
      **/
-    /*@Test
+    @Test
     public void TestConstructora() {
         System.out.println("Test Constructora");
         String nombre = "nombre";
         AsociacionTextos asociacionTextos = new AsociacionTextos("Asociaciondetextos");
-        Alfabeto alfabeto = new Alfabeto("Alfabeto");
-        Algoritmo algoritmo = Algoritmo.QAP;
+        Alfabeto alfabeto = new Alfabeto();
+        //Algoritmo algoritmo = Algoritmo.QAP;
         PairInt dimensiones = new PairInt(10, 20);
 
-        Teclado teclado = new Teclado(nombre, asociacionTextos, alfabeto, algoritmo, dimensiones);
+        Teclado teclado = new Teclado(nombre, asociacionTextos, alfabeto, dimensiones);
 
         assertEquals(nombre, teclado.getNombre());
         //assertEquals(algoritmo, teclado.getAlgoritmo());
         assertEquals(dimensiones, teclado.getDimensiones());
         assertEquals(alfabeto.getNombre(), teclado.getAlfabetoVinculado());
         assertEquals(asociacionTextos.getNombre(), teclado.getAsociacionTextosVinculado());
-    }*/
+    }
 
     ///// GETTERS
     /**
@@ -54,15 +42,15 @@ public class TestTeclado {
      * Operativa: Se comprueba que el método getDimensiones() proporciona los datos correctos sobre las dimensiones del teclado
      **/
 
-    /*@Test
+    @Test
     public void testgetDimensiones() {
         System.out.println("Test get Dimensiones");
         String nombre = "nombre";
         AsociacionTextos asociacionTextos = new AsociacionTextos("Asociaciondetextos");
-        Alfabeto alfabeto = new Alfabeto("Alfabeto");
-        Algoritmo algoritmo = Algoritmo.QAP;
+        Alfabeto alfabeto = new Alfabeto();
+        //Algoritmo algoritmo = Algoritmo.QAP;
         PairInt dimensiones = new PairInt(5, 10);
-        Teclado teclado = new Teclado(nombre, asociacionTextos, alfabeto, algoritmo, dimensiones);
+        Teclado teclado = new Teclado(nombre, asociacionTextos, alfabeto, dimensiones);
 
         teclado.setDimensiones(dimensiones);
 
@@ -70,7 +58,7 @@ public class TestTeclado {
 
         assertEquals(5, resultado.getPrimero().intValue());
         assertEquals(10, resultado.getSegundo().intValue());
-    }*/
+    }
 
     /////SETTERS
     /**
@@ -79,21 +67,21 @@ public class TestTeclado {
      * Valores estudiados: Estrategia caja gris. Se crea un teclado y una puntuación y se comprueba que se puede añadir la segunda a la primera correctamente
      * Operativa: Se comprueba que la puntuación insertada es equivalente a la del teclado tras haberle añadido la misma
      **/
-    /*@Test
+    @Test
     public void testSetPuntuacion() {
         System.out.println("Test set Puntuacion");
         String nombre = "nombre";
         AsociacionTextos asociacionTextos = new AsociacionTextos("Asociaciondetextos");
-        Alfabeto alfabeto = new Alfabeto("Alfabeto");
-        Algoritmo algoritmo = Algoritmo.QAP;
+        Alfabeto alfabeto = new Alfabeto();
+        //Algoritmo algoritmo = Algoritmo.QAP;
         PairInt dimensiones = new PairInt(5, 10);
-        Teclado teclado = new Teclado(nombre, asociacionTextos, alfabeto, algoritmo, dimensiones);
+        Teclado teclado = new Teclado(nombre, asociacionTextos, alfabeto, dimensiones);
         float puntuacion = 100;
 
         teclado.setPuntuacion(puntuacion);
 
         assertEquals(100, teclado.getPuntuacion());
-    }*/
+    }
 
     /**
      * Objeto de la prueba: Test del método setDimensiones(PairInt Dimensiones) de la clase Teclado
@@ -101,22 +89,22 @@ public class TestTeclado {
      * Valores estudiados: Estrategia caja gris. Se crea un teclado y unas dimensiones y se comprueba que se puede añadir la segunda a la primera correctamente
      * Operativa: Se comprueba que las dimensiones insertadas son equivalentes a las del teclado tras haberle añadido las mismas.
      **/
-    /*@Test
+    @Test
     public void testSetDimensiones() {
         System.out.println("Test set Dimensiones");
         String nombre = "nombre";
         AsociacionTextos asociacionTextos = new AsociacionTextos("Asociaciondetextos");
-        Alfabeto alfabeto = new Alfabeto("Alfabeto");
-        Algoritmo algoritmo = Algoritmo.QAP;
+        Alfabeto alfabeto = new Alfabeto();
+        //Algoritmo algoritmo = Algoritmo.QAP;
         PairInt dimensiones = new PairInt(10, 20);
-        Teclado teclado = new Teclado(nombre, asociacionTextos, alfabeto, algoritmo, dimensiones);
+        Teclado teclado = new Teclado(nombre, asociacionTextos, alfabeto, dimensiones);
         PairInt dimensiones2 = new PairInt(5, 10);
 
         teclado.setDimensiones(dimensiones2);
 
         assertEquals(5, teclado.getDimensiones().getPrimero().intValue());
         assertEquals(10, teclado.getDimensiones().getSegundo().intValue());
-    }*/
+    }
 
     /**
      * Objeto de la prueba: Test del método agregarAlfabetoVinculado(String nomA) de la clase Teclado
@@ -124,21 +112,21 @@ public class TestTeclado {
      * Valores estudiados: Estrategia caja gris. Se crea un teclado y un alfabeto, se añade el alfabeto como vinculado al teclado y se comprueba de que se ha agregado correctamente.
      * Operativa: Se comprueba que el mismo método añade el alfabeto al Teclado correctamente.
      **/
-    /*@Test
+    @Test
     public void testagregarAlfabetovinculado() {
         System.out.println("Test agregar Alfabeto Vinculado");
         String nombre = "nombre";
         AsociacionTextos asociacionTextos = new AsociacionTextos("Asociaciondetextos");
-        Alfabeto alfabeto = new Alfabeto("Alfabeto");
-        Algoritmo algoritmo = Algoritmo.QAP;
+        Alfabeto alfabeto = new Alfabeto();
+        //Algoritmo algoritmo = Algoritmo.QAP;
         PairInt dimensiones = new PairInt(10, 20);
-        Teclado teclado = new Teclado(nombre, asociacionTextos, alfabeto, algoritmo, dimensiones);
-        Alfabeto alfabeto2 = new Alfabeto("nombre2");
+        Teclado teclado = new Teclado(nombre, asociacionTextos, alfabeto, dimensiones);
+        Alfabeto alfabeto2 = new Alfabeto();
 
         teclado.agregarAlfabetoVinculado("nombre2");
 
         assertEquals("nombre2", teclado.getAlfabetoVinculado());
-    }*/
+    }
 
     /**
      * Objeto de la prueba: Test del método agregarAsociacionTextosVinculado(String nomAT) de la clase Teclado
@@ -146,21 +134,21 @@ public class TestTeclado {
      * Valores estudiados: Estrategia caja gris. Se crea un teclado y una asociacion de textos, se añade la asociacion como vinculada al teclado y se comprueba de que se ha agregado correctamente.
      * Operativa: Se comprueba que el mismo método añade la Asociación de Textos al Teclado correctamente.
      **/
-    /*@Test
+    @Test
     public void testagregarAsociacionTextosVinculado() {
         System.out.println("Test agregar Asociacion Textos Vinculado");
         String nombre = "nombre";
         AsociacionTextos asociacionTextos = new AsociacionTextos("Asociaciondetextos");
-        Alfabeto alfabeto = new Alfabeto("Alfabeto");
-        Algoritmo algoritmo = Algoritmo.QAP;
+        Alfabeto alfabeto = new Alfabeto();
+        //Algoritmo algoritmo = Algoritmo.QAP;
         PairInt dimensiones = new PairInt(10, 20);
-        Teclado teclado = new Teclado(nombre, asociacionTextos, alfabeto, algoritmo, dimensiones);
+        Teclado teclado = new Teclado(nombre, asociacionTextos, alfabeto, dimensiones);
         AsociacionTextos asociacionTextos2 = new AsociacionTextos("nombre2");
 
         teclado.agregarAsociacionTextosVinculado("nombre2");
 
         assertEquals("nombre2", teclado.getAsociacionTextosVinculado());
-    }*/
+    }
 
     ///// AUXILIARES
 
@@ -170,41 +158,41 @@ public class TestTeclado {
      * Valores estudiados: Estrategia de caja gris. Se crea un teclado con su alfabeto ya creado se comprueba que se puede borrar de la misma.
      * Operativa: Se comprueba que el método borrarAlfabetoVinculado(String nomAT) borra correctamente un String, que corresponde al nombre del alfabeto.
      */
-    /*@Test
+    @Test
     public void testborrarAlfabetoVinculado() {
         System.out.println("Test Borrar Alfabeto Vinculado");
         String nombre = "nombre";
         AsociacionTextos asociacionTextos = new AsociacionTextos("Asociaciondetextos");
-        Alfabeto alfabeto = new Alfabeto("Alfabeto");
-        Algoritmo algoritmo = Algoritmo.QAP;
+        Alfabeto alfabeto = new Alfabeto();
+        //Algoritmo algoritmo = Algoritmo.QAP;
         PairInt dimensiones = new PairInt(10, 20);
 
-        Teclado teclado = new Teclado(nombre, asociacionTextos, alfabeto, algoritmo, dimensiones);
+        Teclado teclado = new Teclado(nombre, asociacionTextos, alfabeto, dimensiones);
 
         teclado.borrarAlfabetoVinculado("Alfabeto");
 
         assertNull(teclado.getAlfabetoVinculado());
-    }*/
+    }
     /**
      * Objeto de la prueba: Test del método borrarAsociacionTextosVinculado(String nomAT) de la clase Teclado.
      * Ficheros de datos necesarios: Datos introducidos manualmente.
      * Valores estudiados: Estrategia de caja gris. Se crea un teclado con su asociaciondeTextos también creada y se comprueba que se puede borrar de la misma.
      * Operativa: Se comprueba que el método borrarAsociacionTextosVinculado(String nomAT) borra correctamente un String, que corresponde al nombre de una asociación de textos.
      */
-    /*@Test
+    @Test
     public void testborrarAsociacionTextosVinculado() {
         System.out.println("Test Borrar Asociacion Textos Vinculado");
         String nombre = "nombre";
         AsociacionTextos asociacionTextos = new AsociacionTextos("Asociaciondetextos");
-        Alfabeto alfabeto = new Alfabeto("Alfabeto");
-        Algoritmo algoritmo = Algoritmo.QAP;
+        Alfabeto alfabeto = new Alfabeto();
+        //Algoritmo algoritmo = Algoritmo.QAP;
         PairInt dimensiones = new PairInt(10, 20);
 
-        Teclado teclado = new Teclado(nombre, asociacionTextos, alfabeto, algoritmo, dimensiones);
+        Teclado teclado = new Teclado(nombre, asociacionTextos, alfabeto, dimensiones);
 
         teclado.borrarAsociacionTextosVinculados("AsociaciondeTextos");
 
         assertNull(teclado.getAlfabetoVinculado());
-    }*/
+    }
 }
 
