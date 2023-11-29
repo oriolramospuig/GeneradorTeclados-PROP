e1053b2a3325d6a229c8140830de49b5d52b91a1
P220191\alexia <alexia@mayorgimenez.cat>
2023-11-29 12:04:53 +0100

    merge creacio classes noves

---
+++ Entrega1/FONTS/src/drivers/DriverPresentacion.java
@@ -0,0 +1,8 @@
+package drivers;
+
+import main.presentation.controllers.CtrlPresentacion;
+public class DriverPresentacion{
+    public static void main(String[] args) {
+        CtrlPresentacion.iniPresentacion();
+    }
+}
---
+++ Entrega1/FONTS/src/main/presentation/controllers/CtrlPresentacion.java
@@ -0,0 +1,18 @@
+package main.presentation.controllers;
+
+
+import main.domain.controllers.CtrlDominio;
+import main.presentation.views.VistaMenuPrincipal;
+
+public class CtrlPresentacion {
+
+    private static final CtrlDominio  cd = new CtrlDominio();
+    public static void iniPresentacion(){
+        VistaMenuPrincipal vmp = new VistaMenuPrincipal();
+    }
+
+
+
+
+
+}
