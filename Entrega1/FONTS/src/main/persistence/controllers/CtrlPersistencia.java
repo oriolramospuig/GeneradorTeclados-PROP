db976bf3382b94f9eab64d79521e1360911a8522
JuliaTena <juliatena@JULIA>
2023-12-01 13:46:51 +0100

    preparar carpeta

---
+++ Entrega1/FONTS/src/main/persistence/controllers/CtrlPersistencia.java
@@ -0,0 +1,9 @@
+package main.persistence.controllers;
+
+/**
+ *  El controlador de persistencia se encarga de gestionar el almacenamiento de datos, es decir, se
+ *  encarga de saber dónde y como están estos almacenados.
+ */
+public class CtrlPersistencia {
+    //private final ;
+}
