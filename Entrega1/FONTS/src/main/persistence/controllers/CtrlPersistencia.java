2e2f7245cd9b4d53667a19cee44d31165396ffb9
JuliaTena <juliatena@JULIA>
2023-12-01 14:37:27 +0100

    gestor alfabeto

--- Entrega1/FONTS/src/main/persistence/controllers/CtrlPersistencia.java
+++ Entrega1/FONTS/src/main/persistence/controllers/CtrlPersistencia.java
@@ -1,9 +1,32 @@
 package main.persistence.controllers;
 
+import main.persistence.classes.GestorAlfabeto;
+
+import java.io.FileNotFoundException;
+
 /**
  *  El controlador de persistencia se encarga de gestionar el almacenamiento de datos, es decir, se
  *  encarga de saber dónde y como están estos almacenados.
  */
 public class CtrlPersistencia {
-    //private final ;
+    private final GestorAlfabeto gestorAlfabetoArchivo;
+
+    //private final GestorTextoArchivo gestorTextoArchivo;
+
+    public CtrlPersistencia() {
+        gestorAlfabetoArchivo = new GestorAlfabeto();
+        //gestorTextoArchivo = new GestorTextoArchivo();
+    }
+
+    public void guardaAlfabeto(String nombreA) throws FileNotFoundException {
+        gestorAlfabetoArchivo.getContenidoAlfabeto(nombreA);
+    }
+
+    public boolean existeAlfabeto(String nombreA) {
+        return gestorAlfabetoArchivo.existeAlfabeto(nombreA);
+    }
+
+    public boolean borraAlfabeto(String nombreA) {
+        return gestorAlfabetoArchivo.borrarAlfabeto(nombreA);
+    }
 }
