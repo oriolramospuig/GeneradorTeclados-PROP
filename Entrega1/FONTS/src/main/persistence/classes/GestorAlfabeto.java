a843d2eb3202572bbf655ae92b8da9be4b4ce231
c0nd3neto <vmorenovillanueva15@gmail.com>
2023-12-02 19:02:13 +0100

    GestorAlfabeto

--- Entrega1/FONTS/src/main/persistence/classes/GestorAlfabeto.java
+++ Entrega1/FONTS/src/main/persistence/classes/GestorAlfabeto.java
@@ -1,54 +1,32 @@
 package main.persistence.classes;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Implementación de la clase gestionadora de datos para la clase "Alfabeto", carga/vuelca los alfabetos modificados en cada caso.
  */
 public class GestorAlfabeto {
-    /**
-     implementación del patrón de diseño "Singleton", con el objetivo de que haya una única instancia de esta clase en el
-     sistema. En este caso, esta propiedad es deseada ya que esta classe no tiene estado (astributos no estáticos). Para
-     lograrlo, declaramos la constructora como privada y añadimos una operación estática que retorne siempre la misma
-     instancia. Para acceder a esta instancia lo haremos mediante la llamada GestorAlfabetoArchivo.getInstance();
-     */
-     private static GestorAlfabeto singletonObject;
+    private static final Logger logger = Logger.getLogger(GestorAlfabeto.class.getName());
+    public static void guardarAlfabeto(Map<String, ArrayList<Character>> alfabetos, String path) {
+        try (ObjectOutputStream o = new ObjectOutputStream(new FileOutputStream(path))) {
+            o.writeObject(alfabetos);
+            System.out.println("Alfabeto guardado con éxito en " + path);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error al guardar alfabeto en" + path, e);
+        }
+    }
 
-     public static GestorAlfabeto getInstance() {
-         if (singletonObject == null)
-             singletonObject = new GestorAlfabeto() {
-             };
-         return singletonObject;
-     }
-
-
-     // ---------- CONSTRUCTORA PRIVADA ----------
-     public GestorAlfabeto() {
-     }
-
-     public List<String> getContenidoAlfabeto(String nombreArchivo) throws FileNotFoundException {
-         LinkedList<String> contenidoAlfabeto = new LinkedList<String>();
-
-         FileReader fr = new FileReader("../INPUT_FILES/"+nombreArchivo);
-         Scanner scan = new Scanner(fr);
-
-         while (scan.hasNextLine()) {
-             contenidoAlfabeto.add(new String(scan.nextLine()));
-         }
-         return contenidoAlfabeto;
-     }
-
-     public boolean existeAlfabeto(String nombreA) {
-       // return alfabetos.containsKey(nombreA);
-         return false;
-     }
-
-     public boolean borrarAlfabeto(String nombreA) {
-         //alfabetos.remove(nombreA);
-         return false;
-     }
+    public static Map<String, ArrayList<Character>> cargarAlfabeto(String path) {
+        Map<String, ArrayList<Character>> alfabeto = new HashMap<>();
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path))) {
+            alfabeto = (Map<String, ArrayList<Character>>) ois.readObject();
+            System.out.println("Alfabeto cargado con éxito desde " + path);
+        } catch (IOException | ClassNotFoundException e) {
+            logger.log(Level.SEVERE, "Error al cargar alfabetos desde el archivo " + path, e);
+        }
+        return alfabeto;
+    }
 }
