99a5e739b9558c6bb24dc6a99ffcb1112bb7f302
oriol.ramos.puig <oriol.ramos.puig@estudiantat.upc.edu>
2023-12-05 10:50:34 +0100

    vistes asociaciones A, B

--- Entrega1/FONTS/src/main/presentation/views/VistaAsociacionTextosA.java
+++ Entrega1/FONTS/src/main/presentation/views/VistaAsociacionTextosA.java
@@ -12,40 +12,28 @@ public class VistaAsociacionTextosA extends JFrame {
     /** Panel donde se incluyen los elementos de la ventana */
     private final JPanel lamina = new JPanel();
     /** Título de media ventana superior */
-    private final JLabel tituloVistaAT1 = new JLabel("Agregar asociación de textos");
-    /** Título de media ventana inferior */
-    private final JLabel tituloVistaAT2 = new JLabel("Borrar asociación de textos");
+    private final JLabel tituloVistaATA = new JLabel("Agregar asociación de textos");
     /** Botón para agregar un alfabeto */
     private final JButton bAgregarAsociacion = new JButton("Agregar asociación de textos");
-    /** Botón para borrar un alfabeto */
-    private final JButton bBorrarAsociacion = new JButton("Borrar asociación de textos");
     /** Botó de tornar a la pantalla del menú principal */
     private final JButton bsalir = new JButton("Atrás");
 
     //VENTANA SUPERIOR
     /** Texto indicando que la barra de texto de al lado es para introducir el nombre de la asociación */
-    private final JLabel txtNombre1 = new JLabel("NOMBRE:");
+    private final JLabel txtNombreATA = new JLabel("NOMBRE:");
     /** Área de texto para introducir el nombre de la asociación que se quiere crear */
-    private final JTextArea areanomAT1 = new JTextArea();
+    private final JTextArea areanomATA = new JTextArea();
     /** Texto indicando que la barra de texto de al lado es para introducir el número de textos que quiere agregar a la asociación */
-    private final JLabel txtNumTextos = new JLabel("NÚM DE TEXTOS A AÑADIR:");
+    private final JLabel txtNumTextosATA = new JLabel("NÚM DE TEXTOS A AÑADIR:");
     /** Área de texto para introducir el número de textos que quiere añadir a la asociación */
-    private final JTextArea areaNumTextos = new JTextArea();
+    private final JTextArea areaNumTextosATA = new JTextArea();
     /** Texto indicando que la barra de texto de al lado es para introducir el contenido de la asociación */
-    private final JLabel txtTextosAgregar = new JLabel("TEXTOS A AGREGAR:");
+    private final JLabel txtTextosAgregarATA = new JLabel("TEXTOS A AGREGAR:");
     /** Área de texto para introducir el contenido de la asociación que se quiere crear */
-    private final JTextArea areaTextosAgregar = new JTextArea();
+    private final JTextArea areaTextosAgregarATA = new JTextArea();
 
-    private JPopupMenu popupMenuA = new JPopupMenu();
-    private JButton showPopupButton = new JButton("Mostrar Menú Emergente");
-
-
-
-    //VENTANA INFERIOR
-    /** Texto indicando que la barra de texto de al lado es para introducir el nombre de la asociación */
-    private final JLabel txtNombre2 = new JLabel("NOMBRE:");
-    /** Área de texto para introducir el nombre de la asociación que se quiere crear */
-    private final JTextArea areanomAT2 = new JTextArea();
+    private JPopupMenu popupMenuATA = new JPopupMenu();
+    private JButton showPopupButtonATA = new JButton("Mostrar Menú Emergente");
 
     //MENSAJES DE ERROR
     /** Pantalla de error que aparece cuando se quiere crear una asociación sin nombre */
@@ -61,56 +49,37 @@ public class VistaAsociacionTextosA extends JFrame {
         //setTitle("Funcionalidades alfabeto);
 
         // Título ventana superior
-        tituloVistaAT1.setBounds(10, 5, 200, 30);
-        add(tituloVistaAT1);
-
-        // Título ventana inferior
-        tituloVistaAT2.setBounds(10, 305, 200, 30);
-        add(tituloVistaAT2);
+        tituloVistaATA.setBounds(10, 5, 200, 30);
+        add(tituloVistaATA);
 
         //VENTANA SUPERIOR
         // Texto Nombre
-        txtNombre1.setBounds(50, 35, 200, 20);
-        add(txtNombre1);
+        txtNombreATA.setBounds(50, 35, 200, 20);
+        add(txtNombreATA);
 
         // Área texto Nombre
-        areanomAT1.setBounds(250,35, 200,20);
-        add(areanomAT1);
+        areanomATA.setBounds(250,35, 200,20);
+        add(areanomATA);
 
         // Texto núm. textos
-        txtNumTextos.setBounds(50, 75, 200, 20);
-        add(txtNumTextos);
+        txtNumTextosATA.setBounds(50, 75, 200, 20);
+        add(txtNumTextosATA);
 
         // Área texto núm. textos
-        areaNumTextos.setBounds(250,75, 200,20);
-        add(areaNumTextos);
+        areaNumTextosATA.setBounds(250,75, 200,20);
+        add(areaNumTextosATA);
 
         // Texto textos a agregar
-        txtTextosAgregar.setBounds(50, 115, 200, 20);
-        add(txtTextosAgregar);
+        txtTextosAgregarATA.setBounds(50, 115, 200, 20);
+        add(txtTextosAgregarATA);
 
         // Área textos a agregar
-        areaTextosAgregar.setBounds(250,115, 200,60);
-        add(areaTextosAgregar);
+        areaTextosAgregarATA.setBounds(250,115, 200,60);
+        add(areaTextosAgregarATA);
 
-
-        //VENTANA INFERIOR
-        // Texto Nombre
-        txtNombre2.setBounds(50, 335, 200, 20);
-        add(txtNombre2);
-
-        // Área texto Nombre
-        areanomAT2.setBounds(250,335, 200,20);
-        add(areanomAT2);
-
-        //
         // Botón agregar Alfabeto
         bAgregarAsociacion.setBounds(700, 250, 250, 20);
         add(bAgregarAsociacion);
-
-        // Botón borrar Alfabeto
-        bBorrarAsociacion.setBounds(700, 400, 200, 20);
-        add(bBorrarAsociacion);
 
         // Botón salir para ir a la pantalla principal
         bsalir.setBounds(800, 500, 100, 20);
@@ -131,13 +100,13 @@ public class VistaAsociacionTextosA extends JFrame {
                     //JPopupMenu popupMenu = createPopupMenu();
 
                     // Obtener la posición del botón y mostrar el menú emergente
-                    int x = showPopupButton.getX();
-                    int y = showPopupButton.getY() + showPopupButton.getHeight();
+                    int x = showPopupButtonATA.getX();
+                    int y = showPopupButtonATA.getY() + showPopupButtonATA.getHeight();
                     //popupMenu.show(showPopupButton, x, y);
                 }
             };
 
-            frame.getContentPane().add(showPopupButton);
+            frame.getContentPane().add(showPopupButtonATA);
             frame.setSize(300, 200);
             frame.setLocationRelativeTo(null);
             frame.setVisible(true);
@@ -169,7 +138,7 @@ public class VistaAsociacionTextosA extends JFrame {
         ActionListener lAgregar = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (areanomAT1.getText().isEmpty()){
+                if (areanomATA.getText().isEmpty()){
                     JDialog sinNombre =  new JDialog(Nomframe, "Error: No Nombre");
                     sinNombre.setBounds(800, 300, 400, 200);
                     sinNombre.setLayout(null);
@@ -192,7 +161,7 @@ public class VistaAsociacionTextosA extends JFrame {
                     };
                     bSalirErrorNombre.addActionListener(lSalirErrorNombre);
 
-                } else if (areaNumTextos.getText().isEmpty()){
+                } else if (areaNumTextosATA.getText().isEmpty()){
                     JDialog sinNumTextos =  new JDialog(CPframe, "Error: No núm textos");
                     sinNumTextos.setBounds(450, 300, 700, 200);
                     sinNumTextos.setLayout(null);
@@ -214,10 +183,10 @@ public class VistaAsociacionTextosA extends JFrame {
                         }
                     };
                     bSalirErrorNumTextos.addActionListener(lSalirErrorConPath);
-                }else if(areaTextosAgregar.getText().isEmpty() ){
+                }else if(areaTextosAgregarATA.getText().isEmpty() ){
                     //&& areaNumTextos.getText() != '0'
 
-                    if(areaTextosAgregar.getText().isEmpty()) {
+                    if(areaTextosAgregarATA.getText().isEmpty()) {
                         //CtrlPresentacion.agregarTexto(areanomA1.getText(), areaContenido.getText());
                         setVisible(false);
                     }
@@ -233,44 +202,6 @@ public class VistaAsociacionTextosA extends JFrame {
             }
         };
 
-        ActionListener lBorrar = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                //CtrlPresentacion.getNombresAlfabetos();
-
-
-                if (areanomAT2.getText().isEmpty()){
-                    JDialog sinNombre2 =  new JDialog(Nomframe, "Error: No Nombre");
-                    sinNombre2.setBounds(800, 300, 400, 200);
-                    sinNombre2.setLayout(null);
-
-                    JLabel txtErrorNombre2 = new JLabel("Hay que entrar el nombre de una asociación de la lista de asociaciones");
-                    txtErrorNombre2.setBounds(10, 20, 400, 40);
-                    JButton bSalirErrorNombre2 = new JButton("Salir");
-                    bSalirErrorNombre2.setVisible(true);
-                    bSalirErrorNombre2.setBounds(150, 110, 100, 30);
-                    sinNombre2.add(txtErrorNombre2);
-                    sinNombre2.add(bSalirErrorNombre2);
-                    sinNombre2.setVisible(true);
-
-                    ActionListener lSalirErrorNombre = new ActionListener() {
-                        @Override
-                        public void actionPerformed(ActionEvent e) {
-                            sinNombre2.dispose();
-                            sinNombre2.setVisible(false);
-                        }
-                    };
-                    bSalirErrorNombre2.addActionListener(lSalirErrorNombre);
-
-                }else { //se han llenado todos los campos
-                    CtrlPresentacion.borrarAlfabeto(areanomAT2.getText());
-                    setVisible(false);
-                }
-
-            }
-        };
-
         ActionListener lSalir = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -279,14 +210,11 @@ public class VistaAsociacionTextosA extends JFrame {
             }
         };
 
-        showPopupButton.addActionListener(e -> popupMenuA.show(showPopupButton, 0, showPopupButton.getHeight()));
+        showPopupButtonATA.addActionListener(e -> popupMenuATA.show(showPopupButtonATA, 0, showPopupButtonATA.getHeight()));
 
 
         bAgregarAsociacion.addActionListener(lAgregar);
-        bBorrarAsociacion.addActionListener(lBorrar);
         bsalir.addActionListener(lSalir);
-
-        // return null;
     }
 }
 
--- Entrega1/FONTS/src/main/presentation/views/VistaAsociacionTextosB.java
+++ Entrega1/FONTS/src/main/presentation/views/VistaAsociacionTextosB.java
@@ -11,41 +11,18 @@ public class VistaAsociacionTextosB extends JFrame {
 
     /** Panel donde se incluyen los elementos de la ventana */
     private final JPanel lamina = new JPanel();
-    /** Título de media ventana superior */
-    private final JLabel tituloVistaAT1 = new JLabel("Agregar asociación de textos");
     /** Título de media ventana inferior */
-    private final JLabel tituloVistaAT2 = new JLabel("Borrar asociación de textos");
-    /** Botón para agregar un alfabeto */
-    private final JButton bAgregarAsociacion = new JButton("Agregar asociación de textos");
+    private final JLabel tituloVistaATB = new JLabel("Borrar asociación de textos");
     /** Botón para borrar un alfabeto */
     private final JButton bBorrarAsociacion = new JButton("Borrar asociación de textos");
     /** Botó de tornar a la pantalla del menú principal */
     private final JButton bsalir = new JButton("Atrás");
 
-    //VENTANA SUPERIOR
-    /** Texto indicando que la barra de texto de al lado es para introducir el nombre de la asociación */
-    private final JLabel txtNombre1 = new JLabel("NOMBRE:");
-    /** Área de texto para introducir el nombre de la asociación que se quiere crear */
-    private final JTextArea areanomAT1 = new JTextArea();
-    /** Texto indicando que la barra de texto de al lado es para introducir el número de textos que quiere agregar a la asociación */
-    private final JLabel txtNumTextos = new JLabel("NÚM DE TEXTOS A AÑADIR:");
-    /** Área de texto para introducir el número de textos que quiere añadir a la asociación */
-    private final JTextArea areaNumTextos = new JTextArea();
-    /** Texto indicando que la barra de texto de al lado es para introducir el contenido de la asociación */
-    private final JLabel txtTextosAgregar = new JLabel("TEXTOS A AGREGAR:");
-    /** Área de texto para introducir el contenido de la asociación que se quiere crear */
-    private final JTextArea areaTextosAgregar = new JTextArea();
-
-    private JPopupMenu popupMenuA = new JPopupMenu();
-    private JButton showPopupButton = new JButton("Mostrar Menú Emergente");
-
-
-
     //VENTANA INFERIOR
     /** Texto indicando que la barra de texto de al lado es para introducir el nombre de la asociación */
-    private final JLabel txtNombre2 = new JLabel("NOMBRE:");
+    private final JLabel txtNombreATB = new JLabel("NOMBRE:");
     /** Área de texto para introducir el nombre de la asociación que se quiere crear */
-    private final JTextArea areanomAT2 = new JTextArea();
+    private final JTextArea areanomATB = new JTextArea();
 
     //MENSAJES DE ERROR
     /** Pantalla de error que aparece cuando se quiere crear una asociación sin nombre */
@@ -60,53 +37,18 @@ public class VistaAsociacionTextosB extends JFrame {
         //setResizable(true);
         //setTitle("Funcionalidades alfabeto);
 
-        // Título ventana superior
-        tituloVistaAT1.setBounds(10, 5, 200, 30);
-        add(tituloVistaAT1);
-
         // Título ventana inferior
-        tituloVistaAT2.setBounds(10, 305, 200, 30);
-        add(tituloVistaAT2);
-
-        //VENTANA SUPERIOR
-        // Texto Nombre
-        txtNombre1.setBounds(50, 35, 200, 20);
-        add(txtNombre1);
-
-        // Área texto Nombre
-        areanomAT1.setBounds(250,35, 200,20);
-        add(areanomAT1);
-
-        // Texto núm. textos
-        txtNumTextos.setBounds(50, 75, 200, 20);
-        add(txtNumTextos);
-
-        // Área texto núm. textos
-        areaNumTextos.setBounds(250,75, 200,20);
-        add(areaNumTextos);
-
-        // Texto textos a agregar
-        txtTextosAgregar.setBounds(50, 115, 200, 20);
-        add(txtTextosAgregar);
-
-        // Área textos a agregar
-        areaTextosAgregar.setBounds(250,115, 200,60);
-        add(areaTextosAgregar);
-
+        tituloVistaATB.setBounds(10, 305, 200, 30);
+        add(tituloVistaATB);
 
         //VENTANA INFERIOR
         // Texto Nombre
-        txtNombre2.setBounds(50, 335, 200, 20);
-        add(txtNombre2);
+        txtNombreATB.setBounds(50, 335, 200, 20);
+        add(txtNombreATB);
 
         // Área texto Nombre
-        areanomAT2.setBounds(250,335, 200,20);
-        add(areanomAT2);
-
-        //
-        // Botón agregar Alfabeto
-        bAgregarAsociacion.setBounds(700, 250, 250, 20);
-        add(bAgregarAsociacion);
+        areanomATB.setBounds(250,335, 200,20);
+        add(areanomATB);
 
         // Botón borrar Alfabeto
         bBorrarAsociacion.setBounds(700, 400, 200, 20);
@@ -121,118 +63,6 @@ public class VistaAsociacionTextosB extends JFrame {
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Dynamic Popup Menu Example");
-            //JButton showPopupButton = new JButton("Mostrar Menú Emergente");
-            ActionListener lPopup = new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    // Crear el menú emergente
-                    //JPopupMenu popupMenu = createPopupMenu();
-
-                    // Obtener la posición del botón y mostrar el menú emergente
-                    int x = showPopupButton.getX();
-                    int y = showPopupButton.getY() + showPopupButton.getHeight();
-                    //popupMenu.show(showPopupButton, x, y);
-                }
-            };
-
-            frame.getContentPane().add(showPopupButton);
-            frame.setSize(300, 200);
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
-        });
-
-        // Método para crear el menú emergente con elementos dinámicos
-        /*private static JPopupMenu createPopupMenu() {
-            JPopupMenu popupMenu = new JPopupMenu();
-            //Pedir a ctrlPresentacion la lista de nombres de los textos existentes
-            ArrayList<String> nombresTextos = CtrlPresentacion.getNombresTextos();
-
-            // Agregar elementos al menú emergente
-            for (int i = 0; i < nombresTextos.size(); ++i) {
-                JMenuItem menuItem = new JMenuItem(nombresTextos.get(i));
-                menuItem.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        JMenuItem source = (JMenuItem) e.getSource();
-                        JOptionPane.showMessageDialog(null, "Seleccionaste: " + source.getText());
-                    }
-                });
-                popupMenu.add(menuItem);
-            }
-            return popupMenu;
-        };*/
-        //showPopupButton.lPopup();
-
-
-        ActionListener lAgregar = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (areanomAT1.getText().isEmpty()){
-                    JDialog sinNombre =  new JDialog(Nomframe, "Error: No Nombre");
-                    sinNombre.setBounds(800, 300, 400, 200);
-                    sinNombre.setLayout(null);
-
-                    JLabel txtErrorNombre = new JLabel("Hay que agregar la asociación con un nombre nuevo");
-                    txtErrorNombre.setBounds(80, 20, 400, 40);
-                    JButton bSalirErrorNombre = new JButton("Salir");
-                    bSalirErrorNombre.setVisible(true);
-                    bSalirErrorNombre.setBounds(150, 110, 100, 30);
-                    sinNombre.add(txtErrorNombre);
-                    sinNombre.add(bSalirErrorNombre);
-                    sinNombre.setVisible(true);
-
-                    ActionListener lSalirErrorNombre = new ActionListener() {
-                        @Override
-                        public void actionPerformed(ActionEvent e) {
-                            sinNombre.dispose();
-                            sinNombre.setVisible(false);
-                        }
-                    };
-                    bSalirErrorNombre.addActionListener(lSalirErrorNombre);
-
-                } else if (areaNumTextos.getText().isEmpty()){
-                    JDialog sinNumTextos =  new JDialog(CPframe, "Error: No núm textos");
-                    sinNumTextos.setBounds(450, 300, 700, 200);
-                    sinNumTextos.setLayout(null);
-
-                    JLabel txtErrorSinNum = new JLabel("Hay que entrar un número de textos que se querrán agregar a la asociación");
-                    txtErrorSinNum.setBounds(20, 20, 700, 40);
-                    JButton bSalirErrorNumTextos = new JButton("Salir");
-                    bSalirErrorNumTextos.setVisible(true);
-                    bSalirErrorNumTextos.setBounds(150, 110, 100, 30);
-                    sinNumTextos.add(txtErrorSinNum);
-                    sinNumTextos.add(bSalirErrorNumTextos);
-                    sinNumTextos.setVisible(true);
-
-                    ActionListener lSalirErrorConPath = new ActionListener() {
-                        @Override
-                        public void actionPerformed(ActionEvent e) {
-                            sinNumTextos.dispose();
-                            sinNumTextos.setVisible(false);
-                        }
-                    };
-                    bSalirErrorNumTextos.addActionListener(lSalirErrorConPath);
-                }else if(areaTextosAgregar.getText().isEmpty() ){
-                    //&& areaNumTextos.getText() != '0'
-
-                    if(areaTextosAgregar.getText().isEmpty()) {
-                        //CtrlPresentacion.agregarTexto(areanomA1.getText(), areaContenido.getText());
-                        setVisible(false);
-                    }
-                }else {//se han llenado todos los campos
-
-                    //CtrlPresentacion.agregarAsociacion(areanomA1.getText(), areaContenido.getText());
-                }
-
-                //if (areaFiles.getText().length() == 0) files = 50;
-                //else files = Integer.parseInt(areaFiles.getText());
-
-
-            }
-        };
-
         ActionListener lBorrar = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -240,7 +70,7 @@ public class VistaAsociacionTextosB extends JFrame {
                 //CtrlPresentacion.getNombresAlfabetos();
 
 
-                if (areanomAT2.getText().isEmpty()){
+                if (areanomATB.getText().isEmpty()){
                     JDialog sinNombre2 =  new JDialog(Nomframe, "Error: No Nombre");
                     sinNombre2.setBounds(800, 300, 400, 200);
                     sinNombre2.setLayout(null);
@@ -264,7 +94,7 @@ public class VistaAsociacionTextosB extends JFrame {
                     bSalirErrorNombre2.addActionListener(lSalirErrorNombre);
 
                 }else { //se han llenado todos los campos
-                    CtrlPresentacion.borrarAlfabeto(areanomAT2.getText());
+                    CtrlPresentacion.borrarAlfabeto(areanomATB.getText());
                     setVisible(false);
                 }
 
@@ -279,14 +109,9 @@ public class VistaAsociacionTextosB extends JFrame {
             }
         };
 
-        showPopupButton.addActionListener(e -> popupMenuA.show(showPopupButton, 0, showPopupButton.getHeight()));
-
-
-        bAgregarAsociacion.addActionListener(lAgregar);
         bBorrarAsociacion.addActionListener(lBorrar);
         bsalir.addActionListener(lSalir);
 
-        // return null;
     }
 }
 
