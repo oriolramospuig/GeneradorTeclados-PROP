8e8095c8e3c8f9bd5c8c09119e8c118bb446fb40
oriol.ramos.puig <oriol.ramos.puig@estudiantat.upc.edu>
2023-12-05 11:41:28 +0100

    vistes texto C, M

--- Entrega1/FONTS/src/main/presentation/views/VistaTextoC.java
+++ Entrega1/FONTS/src/main/presentation/views/VistaTextoC.java
@@ -12,17 +12,13 @@ public class VistaTextoC extends JFrame {
     /** Panel donde se incluyen los elementos de la ventana */
     private final JPanel lamina = new JPanel();
     /** Título de media ventana superior */
-    private final JLabel tituloVistaTxtCM1 = new JLabel("Consultar texto");
+    private final JLabel tituloVistaTxtC = new JLabel("Consultar texto");
     /** Título de media ventana */
-    private final JLabel tituloVistaTxtCM2 = new JLabel("Consultar lista de textos");
-    /** Título de media ventana inferior */
-    private final JLabel tituloVistaTxtCM3 = new JLabel("Modificar texto");
+    private final JLabel tituloVistaTxtC2 = new JLabel("Consultar lista de textos");
     /** Botón para consultar un texto */
     private final JButton bConsultarTexto = new JButton("Consultar texto");
     /** Botón para consultar la lista de textos */
     private final JButton bConsultarListaTextos = new JButton("Consultar lista de textos");
-    /** Botón para modificar un texto */
-    private final JButton bModificarTextos = new JButton("Modificar texto");
     /** Botó de tornar a la pantalla del menú principal */
     private final JButton bsalir = new JButton("Atrás");
 
@@ -30,15 +26,9 @@ public class VistaTextoC extends JFrame {
     //TEXTOS Y AREAS DE TEXTO
     //VENTANA SUPERIOR
     /** Texto indicando que la barra de texto de al lado es para introducir el nombre del texto a consultar*/
-    private final JLabel txtNombreCM1 = new JLabel("NOMBRE:");
+    private final JLabel txtNombreTxtC = new JLabel("NOMBRE:");
     /** Área de texto para introducir el nombre del texto que se quiere consultar */
-    private final JTextArea areanomCMTxt1 = new JTextArea();
-
-    //VENTANA INFERIOR
-    /** Texto indicando que la barra de texto de al lado es para introducir el nombre del texto a modificar */
-    private final JLabel txtNombreCM2 = new JLabel("NOMBRE:");
-    /** Área de texto para introducir el nombre del texto que se quiere modificar */
-    private final JTextArea areanomCMTxt2 = new JTextArea();
+    private final JTextArea areanomTxtC = new JTextArea();
 
     //MENSAJES DE ERROR
     /** Pantalla de error que aparece cuando se quiere consultar/modificar un texto sin nombre */
@@ -54,34 +44,21 @@ public class VistaTextoC extends JFrame {
         //setTitle("Funcionalidades texto);
 
         // Título ventana superior
-        tituloVistaTxtCM1.setBounds(10, 5, 120, 30);
-        add(tituloVistaTxtCM1);
+        tituloVistaTxtC.setBounds(10, 5, 120, 30);
+        add(tituloVistaTxtC);
 
         // Título media ventana
-        tituloVistaTxtCM2.setBounds(10, 205, 200, 30);
-        add(tituloVistaTxtCM2);
-
-        // Título ventana inferior
-        tituloVistaTxtCM3.setBounds(10, 305, 120, 30);
-        add(tituloVistaTxtCM3);
+        tituloVistaTxtC2.setBounds(10, 205, 200, 30);
+        add(tituloVistaTxtC2);
 
         //VENTANA SUPERIOR
         // Texto Nombre
-        txtNombreCM1.setBounds(50, 35, 200, 20);
-        add(txtNombreCM1);
+        txtNombreTxtC.setBounds(50, 35, 200, 20);
+        add(txtNombreTxtC);
 
         // Área texto Nombre
-        areanomCMTxt1.setBounds(250,35, 200,20);
-        add(areanomCMTxt1);
-
-        //VENTANA INFERIOR
-        // Texto Nombre
-        txtNombreCM2.setBounds(50, 335, 200, 20);
-        add(txtNombreCM2);
-
-        // Área texto Nombre
-        areanomCMTxt2.setBounds(250,335, 200,20);
-        add(areanomCMTxt2);
+        areanomTxtC.setBounds(250,35, 200,20);
+        add(areanomTxtC);
 
         // Botón consultar texto
         bConsultarTexto.setBounds(700, 150, 200, 20);
@@ -90,10 +67,6 @@ public class VistaTextoC extends JFrame {
         // Botón consultar lista de textos
         bConsultarListaTextos.setBounds(700, 250, 200, 20);
         add(bConsultarListaTextos);
-
-        // Botón borrar texto
-        bModificarTextos.setBounds(700, 400, 200, 20);
-        add(bModificarTextos);
 
         // Botón salir para ir a la pantalla principal
         bsalir.setBounds(800, 500, 100, 20);
@@ -110,7 +83,7 @@ public class VistaTextoC extends JFrame {
 
                 //CtrlPresentacion.getNombresTextos();
 
-                if (areanomCMTxt1.getText().isEmpty()){
+                if (areanomTxtC.getText().isEmpty()){
                     JDialog sinNombre2 =  new JDialog(Nomframe, "Error: No Nombre");
                     sinNombre2.setBounds(800, 300, 400, 200);
                     sinNombre2.setLayout(null);
@@ -134,7 +107,7 @@ public class VistaTextoC extends JFrame {
                     bSalirErrorNombre2.addActionListener(lSalirErrorNombre);
 
                 }else { //se han llenado todos los campos
-                    CtrlPresentacion.consultarContenidoTexto(areanomCMTxt1.getText());
+                    CtrlPresentacion.consultarContenidoTexto(areanomTxtC.getText());
                     setVisible(false);
                 }
 
@@ -148,44 +121,6 @@ public class VistaTextoC extends JFrame {
             }
         };
 
-        ActionListener lModificarA = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                //CtrlPresentacion.getNombresTextos();
-
-                if (areanomCMTxt2.getText().isEmpty()){
-                    JDialog sinNombre2 =  new JDialog(Nomframe, "Error: No Nombre");
-                    sinNombre2.setBounds(800, 300, 400, 200);
-                    sinNombre2.setLayout(null);
-
-                    JLabel txtErrorNombre2 = new JLabel("Hay que entrar el nombre de un texto de la lista de textos");
-                    txtErrorNombre2.setBounds(10, 20, 400, 40);
-                    JButton bSalirErrorNombre2 = new JButton("Salir");
-                    bSalirErrorNombre2.setVisible(true);
-                    bSalirErrorNombre2.setBounds(150, 110, 100, 30);
-                    sinNombre2.add(txtErrorNombre2);
-                    sinNombre2.add(bSalirErrorNombre2);
-                    sinNombre2.setVisible(true);
-
-                    ActionListener lSalirErrorNombre = new ActionListener() {
-                        @Override
-                        public void actionPerformed(ActionEvent e) {
-                            sinNombre2.dispose();
-                            sinNombre2.setVisible(false);
-                        }
-                    };
-                    bSalirErrorNombre2.addActionListener(lSalirErrorNombre);
-
-                }else { //se han llenado todos los campos
-                    CtrlPresentacion.consultarContenidoTexto(areanomCMTxt2.getText());
-                    setVisible(false);
-                }
-
-            }
-        };
-
-
         ActionListener lSalir = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -196,7 +131,6 @@ public class VistaTextoC extends JFrame {
 
         bConsultarTexto.addActionListener(lConsultarA);
         bConsultarListaTextos.addActionListener(lConsultarListaA);
-        bModificarTextos.addActionListener(lModificarA);
         bsalir.addActionListener(lSalir);
 
     }
