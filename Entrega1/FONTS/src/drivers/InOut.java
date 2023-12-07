ce5b0de23ada6479c7cc500a6ccc2a543f1988c6
alexia <alexia.mayor@estudiantat.upc.edu>
2023-12-07 15:35:58 +0100

    ArreglarAgregar

--- Entrega1/FONTS/src/drivers/InOut.java
+++ Entrega1/FONTS/src/drivers/InOut.java
@@ -1,10 +1,12 @@
-package main.domain.classes.functions;
+package drivers;
 
-import java.io.*;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 /** Clase para gestionar entrada/salida de la terminal
  * @author Oriol Ramos Puig (oriol.ramos.puig@estudiantat.upc.edu)
@@ -51,11 +53,10 @@ public class InOut {
 
     /**
      * Convierte una cadena de texto en una lista de caracteres.
-     *
      * @param line La cadena de texto que se convertirá.
      * @return Una lista de caracteres que representa la cadena de texto.
      */
-    public ArrayList<Character> leerCaracteresDeTerminal(String line) {
+    public static ArrayList<Character> leerCaracteresDeTerminal(String line) {
         ArrayList<Character> caracteres = new ArrayList<>();
         for (char c : line.toCharArray()) {
             caracteres.add(c);
@@ -71,7 +72,7 @@ public class InOut {
      * @throws IOException Si ocurre un error al leer el archivo.
      * @throws IllegalArgumentException Si el contenido del archivo no es válido.
      */
-    public ArrayList<Character> leerCaracteresDeArchivo(String nombreArchivo) throws IOException {
+    public static ArrayList<Character> leerCaracteresDeArchivo(String nombreArchivo) throws IOException {
         String currentDirectory = System.getProperty("user.dir");
         // Ruta absoluta del archivo
         String rutaArchivo = currentDirectory+"\\Entrega1\\data\\Alfabetos\\"+nombreArchivo;
@@ -147,7 +148,7 @@ public class InOut {
      * @param entrada La cadena de entrada a validar.
      * @return true si la entrada es válida, false de lo contrario.
      */
-    public boolean alfabetoValido(String entrada) {
+    public static boolean alfabetoValido(String entrada) {
         if (entrada == null || entrada.isEmpty()) {
             return false; // Entrada vacía no es válida.
         }
