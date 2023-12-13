9970a8dc54cc9e3f1e49ceff8d683566b8fbae2a
oriol.ramos.puig <oriol.ramos.puig@estudiantat.upc.edu>
2023-12-13 11:36:44 +0100

    DriverInteractivo

--- Entrega1/FONTS/src/drivers/QAPTester.java
+++ Entrega1/FONTS/src/drivers/QAPTester.java
@@ -1,35 +1,79 @@
 package drivers;
 import main.domain.classes.functions.QAP;
 import main.domain.classes.functions.SimulatedAnnealing;
-import main.domain.classes.types.PairInt;
 
 import java.io.*;
-import java.nio.file.*;
-import java.util.*;
+import java.util.Scanner;
 
-import static java.lang.Math.sqrt;
-
-/**
- * @author Oriol Ramos Puig (oriol.ramos.puig@estudiantat.upc.edu)
- */
 public class QAPTester {
-
     public static void main(String[] args) {
-        String nombreTest = "sko100a.dat";
-        //String path = "C:\\Users\\34696\\OneDrive\\Descargas\\VARIS\\";
-        //String pathInput = path + "qapdata\\rou12.dat";
-        //String pathSolutions = path + "qapsoln";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Selecciona un test: \n1: sko100a \n2: sko64 \n3: chr12a \n4: chr12b \n5: chr12c \n6: tai10a \n7: tai10b \n8: tai25a");
+        int opcionTest = obtenerOpcionValida(scanner, 8); // Asegúrate de cambiar el segundo argumento al número total de opciones
+
+        String nombreTest = obtenerNombreTest(opcionTest);
+
+        System.out.println("Selecciona un método: \n1: QAP \n2: SA (Simulated Annealing) \n3: N SA (Múltiples SA)");
+        int opcionMetodo = obtenerOpcionValida(scanner, 3);
+
+        String filePath = obtenerRutaArchivo(nombreTest);
+        if (filePath != null) {
+            ejecutarMetodoSeleccionado(filePath, opcionMetodo, scanner);
+        } else {
+            System.out.println("Archivo no encontrado.");
+        }
+    }
+
+    private static int obtenerOpcionValida(Scanner scanner, int maxOpcion) {
+        int opcion;
+        do {
+            while (!scanner.hasNextInt()) {
+                System.out.println("Por favor, introduce un número válido.");
+                scanner.next(); // para descartar la entrada incorrecta
+            }
+            opcion = scanner.nextInt();
+            if (opcion < 1 || opcion > maxOpcion) {
+                System.out.println("Opción inválida, por favor intenta de nuevo.");
+            }
+        } while (opcion < 1 || opcion > maxOpcion);
+        return opcion;
+    }
+
+    private static String obtenerNombreTest(int opcionTest) {
+        switch (opcionTest) {
+            case 1:
+                return "sko100a";
+            case 2:
+                return "sko64";
+            case 3:
+                return "chr12a";
+            case 4:
+                return "chr12b";
+            case 5:
+                return "chr12c";
+            case 6:
+                return "tai10a";
+            case 7:
+                return "tai10b";
+            case 8:
+                return "tai25a";
+            // Agrega más casos para otras opciones
+            default:
+                return null;
+        }
+    }
+
+    private static String obtenerRutaArchivo(String nombreTest) {
         String currentDirectory = System.getProperty("user.dir");
-        // Ruta absoluta del archivo
-        String filePath = currentDirectory+"\\subgrup-prop14.3\\Entrega1\\FONTS\\src\\test\\QAP_tests\\"+nombreTest;
+        return currentDirectory + "\\Entrega1\\FONTS\\src\\test\\QAP_tests\\" + nombreTest + ".dat";
+    }
 
-
-        System.out.println("PATH: " + filePath);
+    private static void ejecutarMetodoSeleccionado(String filePath, int opcionMetodo, Scanner scanner) {
         try {
             BufferedReader br = new BufferedReader(new FileReader(filePath));
-            Scanner scanner = new Scanner(br);
-            int n = scanner.nextInt();
-            System.out.println("EMPEZAMOS! N: " + n);
+            Scanner fileScanner = new Scanner(br);
+            int n = fileScanner.nextInt();
             // Crear matrices de frecuencias y distancias
             int[][] matrizFrecuencias = new int[n][n];
             int[][] matrizDistancias = new int[n][n];
@@ -37,8 +81,8 @@ public class QAPTester {
             // Leer y llenar la matriz de frecuencias
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (scanner.hasNextInt()) { // Asegúrate de que hay un entero disponible antes de leerlo
-                        matrizFrecuencias[i][j] = scanner.nextInt();
+                    if (fileScanner.hasNextInt()) { // Asegúrate de que hay un entero disponible antes de leerlo
+                        matrizFrecuencias[i][j] = fileScanner.nextInt();
                     }
                 }
             }
@@ -46,12 +90,14 @@ public class QAPTester {
             // Leer y llenar la matriz de distancias
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (scanner.hasNextInt()) { // Asegúrate de que hay un entero disponible antes de leerlo
-                        matrizDistancias[i][j] = scanner.nextInt();
+                    if (fileScanner.hasNextInt()) { // Asegúrate de que hay un entero disponible antes de leerlo
+                        matrizDistancias[i][j] = fileScanner.nextInt();
                     }
                 }
             }
-            Integer x = 1;
+
+            fileScanner.close();
+            /*Integer x = 1;
             for (int filas = 1; filas <= n; filas++) {
                 if (n%filas == 0) {
                     int columnas = n/filas;
@@ -62,48 +108,47 @@ public class QAPTester {
                     else System.out.println();
                     x++;
                 }
-            }
-
-            double raiz = sqrt(n);
-            int r = (int)raiz;
+            }*/
             int nfilas = 1, ncolumnas = n;
-            /*if(raiz == r) {
-                nfilas = ncolumnas = r;
+
+            switch (opcionMetodo) {
+                case 1: // QAP
+                    int [][] tec;
+                    QAP qap = new QAP(nfilas,ncolumnas,matrizFrecuencias,matrizDistancias);
+                    tec = qap.getTeclado();
+                    for (int i = 0; i < nfilas; ++i) {
+                        for (int j = 0; j < ncolumnas; ++j) {
+                            System.out.print(tec[i][j] + " ");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 2: // SA
+                    SimulatedAnnealing simulatedAnnealing = new SimulatedAnnealing(nfilas, ncolumnas, matrizFrecuencias, matrizDistancias);
+                    int p = simulatedAnnealing.getPuntuacionFinal();
+                    System.out.println("Puntuacion = " + p);
+                    break;
+                case 3: // N SA
+                    System.out.println("Introduce el número de ejecuciones para SA:");
+                    int nEjecuciones = scanner.nextInt();
+                    int mejor = 0;
+                    int peor = 999999999;
+                    for (int i = 0; i < nEjecuciones; ++i) {
+                        //System.out.println("Iteracion " + i + " simulated Annealing");
+                        SimulatedAnnealing NsimulatedAnnealing = new SimulatedAnnealing(nfilas, ncolumnas, matrizFrecuencias, matrizDistancias);
+                        int pt = NsimulatedAnnealing.getPuntuacionFinal();
+                        //System.out.println("Puntuacion = " + pt);
+                        if (pt > mejor) mejor = pt;
+                        if (pt < peor) peor = pt;
+                    }
+                    System.out.println("Mejor de las N iteraciones = " + mejor);
+                    System.out.println("Peor de las N ejecuciones = " + peor);
+                    break;
+                default:
+                    System.out.println("Método no reconocido.");
             }
-            else {
-                nfilas = r;
-                ncolumnas = nfilas + 1;
-            }*/
-            System.out.println("Filas: " + nfilas + " Columnas: " + ncolumnas);
-            /*int [][] tec;
-            QAP qap = new QAP(nfilas,ncolumnas,matrizFrecuencias,matrizDistancias);
-            tec = qap.getTeclado();
-            for (int i = 0; i < nfilas; ++i) {
-                for (int j = 0; j < ncolumnas; ++j) {
-                    System.out.print(tec[i][j] + " ");
-                }
-                System.out.println();
-            }*/
-            /*int media = 0;
-            for (int i = 0; i < 100; ++i) {
-                System.out.println("Iteracion " + i + " simulated Annealing");
-                SimulatedAnnealing simulatedAnnealing = new SimulatedAnnealing(nfilas, ncolumnas, matrizFrecuencias, matrizDistancias);
-                int p = simulatedAnnealing.getPuntuacionFinal();
-                System.out.println("Puntuacion = " + p);
-                media += p;
-            }
-            float m = (float) media / 100;
-            System.out.println("Media de las 100 iteraciones = " + m);*/
-
-            SimulatedAnnealing simulatedAnnealing = new SimulatedAnnealing(nfilas, ncolumnas, matrizFrecuencias, matrizDistancias);
-            int p = simulatedAnnealing.getPuntuacionFinal();
-            System.out.println("Puntuacion = " + p);
-
-
-
-
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            System.out.println("Error al leer el archivo: " + e.getMessage());
         }
     }
 }
