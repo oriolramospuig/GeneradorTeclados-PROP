34dfa7eb88d960dc1e28ab63f25b0e41cd5bcfcf
JuliaTena <juliatena@JULIA>
2023-12-16 12:34:39 +0100

    comentarios pairfreq

--- Entrega1/FONTS/src/main/domain/classes/types/PairFrequency.java
+++ Entrega1/FONTS/src/main/domain/classes/types/PairFrequency.java
@@ -1,21 +1,35 @@
 package main.domain.classes.types;
 
 /** Clase que implementa un pair de String, Int
- * @author Júlia Tena (julia.tena-domingo   @estudiantat.upc.edu)
+ * @author Júlia Tena (julia.tena.domingo@estudiantat.upc.edu)
  */
 public class PairFrequency {
+    /** Guarda el conjunto de letras que conforman el pairFrequency */
     String pair;
+
+    /** Guarda el número de veces que aparecen las letars */
     int frequency;
 
+    /**
+     * Crea una instancia de la clase PairFrequency e inicializa los atributos de esta clase con los valores correspondientes
+     * @param pair pareja de letras
+     * @param frequency veces que aparecen las letras
+     */
     public PairFrequency(String pair, int frequency) {
         this.pair = pair;
         this.frequency = frequency;
     }
 
+    /**
+     * @return devuelve la pareja de letras
+     */
     public String getPair() {
         return pair;
     }
 
+    /**
+     * @return devuelve la frecuencia de las letras
+     */
     public int getFrequency() {
         return frequency;
     }
