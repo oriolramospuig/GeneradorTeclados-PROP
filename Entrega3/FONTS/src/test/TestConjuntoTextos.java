fd674de2d6caafa755b80a7f20585be07cb1af71
JuliaTena <juliatena@JULIA>
2023-12-20 21:14:16 +0100

    test cnjt textos

--- Entrega3/FONTS/src/test/TestConjuntoTextos.java
+++ Entrega3/FONTS/src/test/TestConjuntoTextos.java
@@ -11,192 +11,148 @@ import static org.junit.Assert.*;
 import static org.junit.Assert.assertTrue;
 
 /**
+ * Clase de pruebas unitarias para ConjuntoTextos. Evalúa el correcto funcionamiento de la creación y manipulación de objetos ConjuntoTextos.
  * @author Júlia Tena (julia.tena.domingo@estudiantat.upc.edu)
  */
 public class TestConjuntoTextos {
+    /** Instancia de la clase ConjuntoTextos utilizada en los casos de prueba. */
     private ConjuntoTextos textos;
 
-    private Frecuencias tfrecuancias1;
+    /** Instancias de la clase Textos (Palabras y Frecuencias) para datos de prueba. */
+    private Palabras textoPalabras;
+    private Frecuencias textoFrecuencias;
 
-    private Frecuencias tfrecuencias2;
+    /** Constantes para datos de prueba. */
+    private static final String nombrePalabras = "textoPalabras";
+    private static final String nombreFrecuencias = "textoFrecuencias";
 
-    private Palabras tpalabras1;
-
-    private Palabras tpalabras2;
-
+    /**
+     * Inicialización de las instancias Palabras y Frecuencias para realizar los juegos de prueba.
+     */
     @Before
     public void setUp() {
-        textos = new ConjuntoTextos();
-        HashMap<String, Integer> frecuenciaLetras = new HashMap<String,Integer>();
-        HashMap<String, Integer> contenidoFrec = new HashMap<String, Integer>();
+        HashMap<String, Integer> frecuenciaLetras = new HashMap<>();
         frecuenciaLetras.put("ho", 1);
+        frecuenciaLetras.put("ol", 1);
         frecuenciaLetras.put("la", 1);
-        contenidoFrec.put("hola", 2);
-        tfrecuancias1 = new Frecuencias("nombreF1", contenidoFrec, frecuenciaLetras);         //tfrecuencias1 tiene: hola-2
-        frecuenciaLetras.remove("ho", 1);
-        frecuenciaLetras.remove("la", 1);
-        contenidoFrec.remove("hola", 2);
-        contenidoFrec.put("ai", 1);
-        tfrecuencias2 = new Frecuencias("nombreF2", contenidoFrec, frecuenciaLetras);         //tfrecuencias2 tiene ai-1
-        tpalabras1 = new Palabras("nombreP1", "abc", frecuenciaLetras);
-        tpalabras2 = new Palabras("nombreP2", "cde", frecuenciaLetras);
+
+        //Palabras
+        String contenidoPalabras = "hola";
+        textoPalabras = new Palabras(nombrePalabras, contenidoPalabras, frecuenciaLetras);
+
+        //Frecuencias
+        HashMap<String, Integer> frecuenciaPalabras = new HashMap<>();
+        frecuenciaPalabras.put("hola", 1);
+        textoFrecuencias = new Frecuencias(nombreFrecuencias, frecuenciaPalabras, frecuenciaLetras);
     }
 
-
-    // ---------- CONSTRUCTORAS ----------
     /**
-     * Objeto de la prueba: Test de la constructora de ConjuntoTextos (Constructora Default).
-     * Ficheros de datos necesarios: Datos introducidos manualmente. No hacen falta ficheros adicionales.
-     * Valores estudiados: Se inicializa el atributo de conjuntoTextos vacío.
-     * Operativa: AssertEquals() comprueba que la lista de textos está vacía.
+     * Objeto de la prueba: Test de la constructora de ConjuntoTextos.
+     * Ficheros de datos necesarios: No se requieren ficheros externos para esta prueba.
+     * Valores estudiados: Estrategia caja gris. Se define un conjunto de textos y se comprueba que se crea correctamente.
+     * Operativa: Creamos un nuevo ConjuntoTextos. Se comprueba que se ha creado vacío.
      */
     @Test
     public void TestConstructora() {
-        System.out.println("Test Constructora");
+        //System.out.println("Test Constructora");
+        textos = new ConjuntoTextos();
 
         assertTrue(textos.getNombresTextos().isEmpty());
     }
 
-
-    // ---------- GETTERS ----------
     /**
-     * Objeto de la prueba: Test del método GetTexto de ConjuntoTextos.
-     * Ficheros de datos necesarios: Datos introducidos manualmente. No hacen falta ficheros adicionales.
-     * Valores estudiados: Se consulta un objecto texto con el atributo que lo identifica.
-     * Primero se añade un texto para poder consultarlo.
-     * Seguidamente se llama a la función getTexto que retorna el objeto textocon el nombre nombreF1 que es el que pasamos como parámetro.
-     * Operativa :AssertNotNull() compruba que la variable donde se ha guardado el objeto texto consultado no es null.
-     * AssertEquals() comprueba que el nombre del texto guardado equivale al nombre que se le ha añadido inicialmente.
+     * Objeto de la prueba: Test del método getTexto de la clase ConjuntoTextos.
+     * Ficheros de datos necesarios: No se requieren ficheros externos para esta prueba.
+     * Valores estudiados: Estrategia caja negra. Se añaden textos al conjunto y se verifica que el método getTexto devuelve los textos correctos.
+     * Operativa: Se agregan instancias de Palabras y Frecuencias al conjunto. Se llama a getTexto para ambos textos y se verifica la igualdad.
      */
     @Test
     public void TestGetTexto() {
-        System.out.println("Test getTexto");
+        //System.out.println("Test getTexto");
+        textos = new ConjuntoTextos();
+        textos.agregarTexto(nombrePalabras, textoPalabras);
+        textos.agregarTexto(nombreFrecuencias, textoFrecuencias);
 
-        textos.agregarTexto("nombreF1", tfrecuancias1);
-        Texto resultadoFrecuencia = textos.getTexto("nombreF1");
+        Texto texto1 = textos.getTexto(nombrePalabras);
+        Texto texto2 = textos.getTexto(nombreFrecuencias);
 
-        textos.agregarTexto("nombreP1", tpalabras1);
-        Texto resultadoPalabras = textos.getTexto("nombreP1");
-
-        assertNotNull(resultadoFrecuencia);
-        assertEquals("nombreF1", resultadoFrecuencia.getNombre());
-        assertNotNull(resultadoPalabras);
-        assertEquals("nombreP1", resultadoPalabras.getNombre());
+        assertNotNull(texto1);
+        assertEquals(textoPalabras, texto1);
+        assertNotNull(texto2);
+        assertEquals(textoFrecuencias, texto2);
     }
 
+
     /**
-     * Objeto de la prueba: Test del método GetNombresTextos de ConjuntoTextos.
-     * Ficheros de datos necesarios: Datos introducidos manualmente. No hacen falta ficheros adicionales.
-     * Valores estudiados: Se consulta la lista de nombres de los textos existentes en el sistema.
-     * Primero se añaden cuatro textos para poder consultar la lista.
-     * Seguidamente se llama a la función getNombresTextos que retorna la lista de los nombres de los textos existentes
-     * Operativa: AssertEquals() comprueba que la medida de la lista de textos sea 4.
-     * Los siguientes cuatro AssertTrue() comprueban que existan en la lista todos los nombres agregados anteriormente.
+     * Objeto de la prueba: Test del método getNombresTextos de la clase ConjuntoTextos.
+     * Ficheros de datos necesarios: No se requieren ficheros externos para esta prueba.
+     * Valores estudiados: Estrategia caja negra. Se añaden textos al conjunto y se verifica que el método getNombresTextos devuelve los nombres correctos.
+     * Operativa: Se agregan instancias de Palabras y Frecuencias al conjunto. Se llama a getNombresTextos y se verifica la igualdad.
      */
     @Test
     public void TestGetNombresTextos() {
-        System.out.println("Test getNombresTextos");
+        //System.out.println("Test getNombresTextos");
+        textos = new ConjuntoTextos();
 
-        textos.agregarTexto("nombreF1", tfrecuancias1);
-        textos.agregarTexto("nombreF2", tfrecuencias2);
-        textos.agregarTexto("nombreP1", tpalabras1);
-        textos.agregarTexto("nombreP2", tpalabras2);
+        textos.agregarTexto(nombrePalabras, textoPalabras);
+        textos.agregarTexto(nombreFrecuencias, textoFrecuencias);
 
         ArrayList<String> nombres = textos.getNombresTextos();
 
-        assertEquals(4, nombres.size());
-        assertTrue(nombres.contains("nombreF1"));
-        assertTrue(nombres.contains("nombreF2"));
-        assertTrue(nombres.contains("nombreP1"));
-        assertTrue(nombres.contains("nombreP2"));
+        assertEquals(2, nombres.size());
+        assertTrue(nombres.contains(nombrePalabras));
+        assertTrue(nombres.contains(nombreFrecuencias));
     }
 
-
-    // ---------- SETTERS ----------
     /**
-     * Objeto de la prueba: Test del método AgregarTexto de ConjuntoTextos.
-     * Ficheros de datos necesarios: Datos introducidos manualmente. No hacen falta ficheros adicionales.
-     * Valores estudiados: Se añaden cuatro textos con la función agregartexto y con los nombres asignados que es el atributo que los identifica.
-     * Operativa: AssertEquals() comprueba que la medida de la lista de textos sea 4.
-     * Los siguientes AssertTrue() comprueban que los nombres de los textos guardados existen en la lista.
+     * Objeto de la prueba: Test del método agregarTexto de la clase ConjuntoTextos.
+     * Ficheros de datos necesarios: No se requieren ficheros externos para esta prueba.
+     * Valores estudiados: Estrategia caja negra. Se añaden textos al conjunto y se verifica que se agregan correctamente.
+     * Operativa: Se añaden instancias de Palabras y Frecuencias al conjunto. Se verifica que los nombres estén presentes y los textos sean iguales.
      */
     @Test
     public void TestAgregarTexto(){
-        System.out.println("Test agregarTexto");
+        //System.out.println("Test agregarTexto");
+        textos = new ConjuntoTextos();
+        assertTrue(textos.getNombresTextos().isEmpty());
 
-        textos.agregarTexto("nombreF1", tfrecuancias1);
-        textos.agregarTexto("nombreF2", tfrecuencias2);
-        textos.agregarTexto("nombreP1", tpalabras1);
-        textos.agregarTexto("nombreP2", tpalabras2);
+        textos.agregarTexto(nombrePalabras, textoPalabras);
 
-        assertEquals(4, textos.getNombresTextos().size());
-        //assertTrue(textos.getTextos().containsKey("nombreF1"));
-        //assertTrue(textos.getTextos().containsKey("nombreF2"));
-        //assertTrue(textos.getTextos().containsKey("nombreP1"));
-        //assertTrue(textos.getTextos().containsKey("nombreP2"));
-    }
+        assertTrue(textos.getNombresTextos().contains(nombrePalabras));
+        assertEquals(textoPalabras, textos.getTexto(nombrePalabras));
 
+        textos.agregarTexto(nombreFrecuencias, textoFrecuencias);
 
-    // ---------- AUXILIARES ----------
-    /**
-     * Objeto de la prueba: Test del método ExisteTexto de ConjuntoTextos.
-     * Ficheros de datos necesarios: Datos introducidos manualmente. No hacen falta ficheros adicionales.
-     * Valores estudiados: Se consulta si los texto con un nombre concreto existen en la lista de textos existentes.
-     * Operativa: Los AssertTrue() comprueban que el nombre de los textos guardados existen en la lista.
-     * Los AssertFalse() comprueban que los nombres de los textos no guardados, no existen en la lista.
-     */
-    @Test
-    public void TestExisteTexto() {
-        System.out.println("Test existeTexto");
+        assertTrue(textos.getNombresTextos().contains(nombreFrecuencias));
+        assertEquals(textoFrecuencias, textos.getTexto(nombreFrecuencias));
 
-        textos.agregarTexto("nombreF1", tfrecuancias1);
-        textos.agregarTexto("nombreP1", tpalabras1);
-
-        assertTrue(textos.existeTexto("nombreF1"));
-        assertTrue(textos.existeTexto("nombreP1"));
-        assertFalse(textos.existeTexto("nombreF2"));
-        assertFalse(textos.existeTexto("nombreP2"));
+        assertEquals(2, textos.getNombresTextos().size());
     }
 
     /**
-     * Objetivo de la Prueba:
-     * La función `TestBorrarTexto` tiene como objetivo verificar el correcto funcionamiento
-     * del método `borrarTexto` de la clase `Textos`. Este método debería eliminar un texto
-     * específico de la colección.
-     *
-     * Ficheros Necesarios:
-     * No se mencionan ficheros específicos necesarios para esta prueba. Parece depender
-     * principalmente de las estructuras de datos internas de la clase `Textos`.
-     *
-     * Valores Estudiados:
-     * Se evalúan los siguientes aspectos:
-     * 1. Se agregan un total de cuatro textos a la colección mediante el método `agregarTexto`.
-     * 2. Se eliminan dos textos específicos mediante el método `borrarTexto`.
-     * 3. Se verifica que la colección tenga el tamaño esperado después de la eliminación.
-     * 4. Se verifica la no existencia de los textos eliminados y la existencia de los no eliminados.
-     *
-     * Operativa:
-     * 1. Se agregan un total de cuatro textos a la colección mediante el método `agregarTexto`.
-     * 2. Se eliminan dos textos específicos mediante el método `borrarTexto`.
-     * 3. Se verifica que la colección tenga el tamaño esperado después de la eliminación.
-     * 4. Se verifica la no existencia de los textos eliminados y la existencia de los no eliminados.
+     * Objeto de la prueba: Test del método borrarTexto de la clase ConjuntoTextos.
+     * Ficheros de datos necesarios: No se requieren ficheros externos para esta prueba.
+     * Valores estudiados: Estrategia caja negra. Se añaden textos al conjunto, se borra un texto y se verifica que se haya eliminado correctamente.
+     * Operativa: Se añaden instancias de Palabras y Frecuencias al conjunto. Se verifica que los nombres estén presentes y los textos sean iguales.
+     * Luego, se borra un texto y se verifica que el texto se haya eliminado correctamente y los demás textos sigan presentes.
      */
     @Test
     public void TestBorrarTexto() {
-        System.out.println("Test borrarTexto");
+        //System.out.println("Test borrarTexto");
+        textos = new ConjuntoTextos();
 
-        textos.agregarTexto("nombreF1", tfrecuancias1);
-        textos.agregarTexto("nombreF2", tfrecuencias2);
-        textos.agregarTexto("nombreP1", tpalabras1);
-        textos.agregarTexto("nombreP2", tpalabras2);
+        textos.agregarTexto(nombrePalabras, textoPalabras);
+        textos.agregarTexto(nombreFrecuencias, textoFrecuencias);
 
-        textos.borrarTexto("nombreF1");
-        textos.borrarTexto("nombreP1");
+        assertTrue(textos.existeTexto(nombrePalabras));
+        assertTrue(textos.existeTexto(nombreFrecuencias));
 
-        assertEquals(2, textos.getNombresTextos().size());
-        assertFalse(textos.existeTexto("nombreF1"));
-        assertFalse(textos.existeTexto("nombreP1"));
-        assertTrue(textos.existeTexto("nombreF2"));
-        assertTrue(textos.existeTexto("nombreP2"));
+        textos.borrarTexto(nombrePalabras);
+
+        assertFalse(textos.existeTexto(nombrePalabras));
+        assertNull(textos.getTexto(nombrePalabras));
+
+        assertTrue(textos.existeTexto(nombreFrecuencias));
+        assertEquals(textoFrecuencias, textos.getTexto(nombreFrecuencias));
     }
 }
